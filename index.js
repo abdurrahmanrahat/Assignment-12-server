117685186232914ab03e03d0a7cbc691aa9bd906
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-06-12 20:31:34 +0600

    Delete query with id from seletedClasses

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjja2r0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -75,6 +75,13 @@ async function run() {
         app.post('/selectedClasses', async (req, res) => {
             const selectedClass = req.body;
             const result = await selectedClassCollection.insertOne(selectedClass);
+            res.send(result);
+        })
+
+        app.delete('/selectedClasses/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await selectedClassCollection.deleteOne(query);
             res.send(result);
         })
 
